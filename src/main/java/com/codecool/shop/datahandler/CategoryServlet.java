7f3f37e8677adeb52f55310d06bf942cda3a9fa9
Ichy91 <farago.istvan91@gmail.com>
2021-08-31 14:48:17 +0200

    feat: add the back end side for the category sorting.

--- src/main/java/com/codecool/shop/datahandler/CategoryServlet.java
+++ src/main/java/com/codecool/shop/datahandler/CategoryServlet.java
@@ -1,4 +1,31 @@
 package com.codecool.shop.datahandler;
 
-public class CategoryServlet {
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.model.ProductCategory;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CategoryServlet", urlPatterns = "/categories")
+public class CategoryServlet extends HttpServlet {
+
+    ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        ProductCategory category = productCategoryDataStore.find(id);
+
+        PrintWriter out = response.getWriter();
+
+        out.println(category.getProducts());
+    }
 }
