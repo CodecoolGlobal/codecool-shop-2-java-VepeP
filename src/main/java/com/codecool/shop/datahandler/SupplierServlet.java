072042ffdb74a4d48bb7d0a357b4a6782cf18737
Ichy91 <farago.istvan91@gmail.com>
2021-08-31 14:42:27 +0200

    feat: add the back end side for the supplier sorting.

--- src/main/java/com/codecool/shop/datahandler/SupplierServlet.java
+++ src/main/java/com/codecool/shop/datahandler/SupplierServlet.java
@@ -1,4 +1,31 @@
 package com.codecool.shop.datahandler;
 
-public class SupplierServlet {
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Supplier;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SupplierServlet", urlPatterns = "/suppliers")
+public class SupplierServlet extends HttpServlet {
+
+    SupplierDao productSupplierDataStore = SupplierDaoMem.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        Supplier supplier = productSupplierDataStore.find(id);
+
+        PrintWriter out = response.getWriter();
+
+        out.println(supplier.getProducts());
+    }
 }
