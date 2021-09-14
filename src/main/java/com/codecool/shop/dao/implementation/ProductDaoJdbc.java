17d63b65158563edae14581039c1b5c58ccb7b50
VepeP <57757745+VepeP@users.noreply.github.com>
2021-09-14 15:02:09 +0200

    feat: add SupplierDaoJdbc

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -1,75 +1,159 @@
 package com.codecool.shop.dao.implementation;
 
 
+import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ProductDaoMem implements ProductDao {
+public class ProductDaoJdbc implements ProductDao {
+    private final DataSource dataSource;
 
-    private List<Product> products = new ArrayList<>();
-    private static ProductDaoMem instance = null;
-
-    /* A private Constructor prevents any other class from instantiating.
-     */
-    private ProductDaoMem() {
-    }
-
-    public static ProductDaoMem getInstance() {
-        if (instance == null) {
-            instance = new ProductDaoMem();
-        }
-        return instance;
-    }
-
-    public static void setInstance(ProductDaoMem instance) {
-        ProductDaoMem.instance = instance;
+    public ProductDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Override
     public void add(Product product) {
-        product.setId(products.size() + 1);
-        products.add(product);
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "INSERT INTO product (id, default_price, default_currency, category_id, supplier_id, name, description)" +
+                         " VALUES (?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, product.getId());
+            st.setInt(2, product.getDefaultPrice().intValue());
+            st.setString(3, product.getDefaultCurrency().getCurrencyCode());
+            st.setInt(4, product.getProductCategory().getId());
+            st.setInt(5, product.getSupplier().getId());
+            st.setString(6, product.getName());
+            st.setString(7, product.getDescription());
+            st.executeUpdate();
+        } catch (SQLException throwable) {
+            throw new RuntimeException("Error while adding new product.", throwable);
+        }
     }
 
     @Override
     public Product find(int id) {
-        return products.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+        try (Connection conn = dataSource.getConnection()){
+            String sql = "SELECT id, name, default_price, default_currency, description, category_id, supplier_id FROM product WHERE id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+
+            if (!rs.next()) return null;
+            ProductCategoryDaoJdbc productCategoryDaoJdbc = new ProductCategoryDaoJdbc(dataSource);
+            SupplierDaoJdbc supplierDaoJdbc = new SupplierDaoJdbc(dataSource);
+            Product product = new Product(rs.getInt(1),
+                    rs.getString(2),
+                    new BigDecimal(rs.getInt(3)),
+                    rs.getString(4),
+                    rs.getString(5),
+                    productCategoryDaoJdbc.find(rs.getInt(6)),
+                    supplierDaoJdbc.find(rs.getInt(7)));
+
+            return product;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading product with id: " + id, e);
+        }
     }
 
     @Override
     public void remove(int id) {
-        products.remove(find(id));
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "DELETE FROM product WHERE id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            st.executeUpdate();
+        } catch (SQLException throwable) {
+            throw new RuntimeException("Error while deleting product with id: " + id, throwable);
+        }
     }
 
     @Override
     public List<Product> getAll() {
-        return products;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, name, default_price, default_currency, description, category_id, supplier_id FROM product";
+            ResultSet rs = conn.prepareStatement(sql).executeQuery();
+            List<Product> result = new ArrayList<>();
+            ProductCategoryDaoJdbc productCategoryDaoJdbc = new ProductCategoryDaoJdbc(dataSource);
+            SupplierDaoJdbc supplierDaoJdbc = new SupplierDaoJdbc(dataSource);
+            while (rs.next()) {
+                Product product = new Product(rs.getInt(1),
+                        rs.getString(2),
+                        new BigDecimal(rs.getInt(3)),
+                        rs.getString(4),
+                        rs.getString(5),
+                        productCategoryDaoJdbc.find(rs.getInt(6)),
+                        supplierDaoJdbc.find(rs.getInt(7)));
+                result.add(product);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all categories", e);
+        }
     }
 
     @Override
     public List<Product> getBy(Supplier supplier) {
-        List<Product> productList = new ArrayList<>();
-        for (Product product: products)
-            if (product.getSupplier().getId() == supplier.getId()) productList.add(product);
-        return productList;
-        //return products.stream().filter(t -> t.getSupplier().getId() == supplier.getId()).collect(Collectors.toList());
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, name, default_price, default_currency, description, category_id, supplier_id FROM product where supplier_id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, supplier.getId());
+            ResultSet rs = st.executeQuery();
+            List<Product> result = new ArrayList<>();
+            ProductCategoryDaoJdbc productCategoryDaoJdbc = new ProductCategoryDaoJdbc(dataSource);
+            SupplierDaoJdbc supplierDaoJdbc = new SupplierDaoJdbc(dataSource);
+            while (rs.next()) {
+                Product product = new Product(rs.getInt(1),
+                        rs.getString(2),
+                        new BigDecimal(rs.getInt(3)),
+                        rs.getString(4),
+                        rs.getString(5),
+                        productCategoryDaoJdbc.find(rs.getInt(6)),
+                        supplierDaoJdbc.find(rs.getInt(7)));
+                result.add(product);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all categories", e);
+        }
     }
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
-        return products.stream().filter(t -> t.getProductCategory().getId() == (productCategory.getId())).collect(Collectors.toList());
-    }
-
-    @Override
-    public String toString() {
-        return "ProductDaoMem{" +
-                "products=" + products +
-                '}';
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, name, default_price, default_currency, description, category_id, supplier_id FROM product where category_id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, productCategory.getId());
+            ResultSet rs = st.executeQuery();
+            List<Product> result = new ArrayList<>();
+            ProductCategoryDaoJdbc productCategoryDaoJdbc = new ProductCategoryDaoJdbc(dataSource);
+            SupplierDaoJdbc supplierDaoJdbc = new SupplierDaoJdbc(dataSource);
+            while (rs.next()) {
+                Product product = new Product(rs.getInt(1),
+                        rs.getString(2),
+                        new BigDecimal(rs.getInt(3)),
+                        rs.getString(4),
+                        rs.getString(5),
+                        productCategoryDaoJdbc.find(rs.getInt(6)),
+                        supplierDaoJdbc.find(rs.getInt(7)));
+                result.add(product);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all categories", e);
+        }
     }
 }
--- src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
@@ -1,57 +1,84 @@
 package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SupplierDaoMem implements SupplierDao {
+public class SupplierDaoJdbc implements SupplierDao {
 
-    private List<Supplier> suppliers = new ArrayList<>();
-    private static SupplierDaoMem instance = null;
+    private final DataSource dataSource;
 
-    /* A private Constructor prevents any other class from instantiating.
-     */
-    private SupplierDaoMem() {
-    }
-
-    public static SupplierDaoMem getInstance() {
-        if (instance == null) {
-            instance = new SupplierDaoMem();
-        }
-        return instance;
-    }
-
-    public static void setInstance(SupplierDaoMem instance) {
-        SupplierDaoMem.instance = instance;
+    public SupplierDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Override
     public void add(Supplier supplier) {
-        supplier.setId(suppliers.size() + 1);
-        suppliers.add(supplier);
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "INSERT INTO supplier (name, description) VALUES (?, ?)";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, supplier.getName());
+            st.setString(2, supplier.getDescription());
+            st.executeUpdate();
+        } catch (SQLException throwable) {
+            throw new RuntimeException("Error while adding new supplier.", throwable);
+        }
     }
 
     @Override
     public Supplier find(int id) {
-        return suppliers.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+        try (Connection conn = dataSource.getConnection()){
+            String sql = "SELECT id, name, description FROM supplier WHERE id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+
+            if (!rs.next()) return null;
+            Supplier supplier = new Supplier(rs.getInt(1),
+                    rs.getString(2),
+                    rs.getString(3));
+
+            return supplier;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading supplier with id: " + id, e);
+        }
     }
 
     @Override
     public void remove(int id) {
-        suppliers.remove(find(id));
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "DELETE FROM supplier WHERE id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            st.executeUpdate();
+        } catch (SQLException throwable) {
+            throw new RuntimeException("Error while deleting supplier with id: " + id, throwable);
+        }
     }
 
     @Override
     public List<Supplier> getAll() {
-        return suppliers;
-    }
-
-    @Override
-    public String toString() {
-        return "SupplierDaoMem{" +
-                "suppliers=" + suppliers +
-                '}';
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, name, description FROM supplier";
+            ResultSet rs = conn.prepareStatement(sql).executeQuery();
+            List<Supplier> result = new ArrayList<>();
+            while (rs.next()) {
+                Supplier supplier = new Supplier(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3));
+                result.add(supplier);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all suppliers", e);
+        }
     }
 }
